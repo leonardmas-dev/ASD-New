f1830996643d56fb66f5e90ee543e0923017c01a
Leonard Masters <leonardmichaelmasters@hotmail.com>
2026-03-25 12:41:05 +0000

    Redo on tenant_list

--- ui/tenants/tenant_list_page.py
+++ ui/tenants/tenant_list_page.py
@@ -1,12 +1,13 @@
-import sys
-import os
-# Ensures database.models can be read by this file
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
 import tkinter as tk
 from tkinter import ttk, messagebox
+from backend.tenant_service import fetch_tenants
 from database.models import Tenant
 from database.session import get_session
+from tenants_home import TenantsHome
 
+
+'''
 #automatically close the session by using with
 def fetch_tenants():
     with get_session() as session:
@@ -20,7 +21,7 @@ root.geometry("1200x400")
 
 tk.Label(root, text="Tenant Information")
 
-# Displaying the tenant data
+# Displaying the tenant data in tkinter tree
 columns = ("Tenant ID", "First Name", "Last Name", "Phone", "Email", "Apartment")
 tree = ttk.Treeview(root, columns=columns, show="headings")
 
@@ -97,3 +98,68 @@ delete_btn = tk.Button(root, text="Delete Tenant", command=delete_tenant)
 delete_btn.pack(pady=10)
 
 root.mainloop()
+
+'''
+
+
+class TenantList(tk.Frame):
+    def __init__(self, parent, main_window):
+        super().__init__(parent)
+        self.main_window = main_window
+        tenant_list_title = tk.Label(self, text="Tenant Information")
+        tenant_list_title.pack()
+
+        columns = ("Tenant ID", "First Name", "Last Name", "Phone", "Email", "Apartment")
+        self.tree = ttk.Treeview(self, columns=columns, show="headings")
+
+        for col in columns:
+            self.tree.heading(col, text=col)
+            self.tree.column(col, width=100)
+
+        self.tree.pack(fill="both", expand=True)
+
+        scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.tree.yview)
+        self.tree.configure(yscroll=scrollbar.set)
+        scrollbar.pack(side="right", fill="y")
+
+        self.load_data()
+
+        # Delete Tenant button wont work unless a tenant_id is selected
+        delete_btn = tk.Button(self, text="Delete Tenant", command=self.delete_tenant)
+        delete_btn.pack(pady=10)
+
+        go_homebtn = tk.Button(self, text="Go Home", command=lambda: main_window.load_page(TenantsHome))
+        go_homebtn.pack(pady=10)
+
+
+    def load_data(self):
+        for tenant in fetch_tenants():
+            self.tree.insert("", "end", values=(
+                tenant.tenant_id,
+                tenant.first_name,
+                tenant.last_name,
+                tenant.phone,
+                tenant.email,
+                tenant.location_id
+            ))
+
+    def delete_tenant(self):
+        selected_item = self.tree.selection()
+
+
+        if not selected_item:
+            messagebox.showwarning("Warning", "Select a tenant first")
+            return
+
+        tenant_data = self.tree.item(selected_item)["values"]
+        tenant_id = tenant_data[0]
+
+
+
+        with get_session() as session:
+            tenant = session.query(Tenant).filter_by(tenant_id=tenant_id).first()
+            if tenant:
+                session.delete(tenant)
+                session.commit()
+
+        self.tree.delete(selected_item)
--- ui/tenants/tenants_home.py
+++ ui/tenants/tenants_home.py
@@ -1,7 +1,13 @@
 import tkinter as tk
+from tenants.tenant_list_page import TenantList
 
 class TenantsHome(tk.Frame):
     def __init__(self, parent, main_window):
         super().__init__(parent)
+        self.main_window = main_window
 
-        tk.Label(self, text="Tenants", font=("Arial", 18)).pack(pady=40)
+        tk.Label(self, text="Tenants Home Page", font=("Arial", 18)).pack(pady=40)
+        
+
+        
+        tk.Button(self, text="View Tenants", command=lambda: main_window.load_page(TenantList)).pack(pady=20)
